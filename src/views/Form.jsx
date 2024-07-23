5618dc5d3ae2947b19c6a084a374293bf38bc09a
pnxu <fcaavv@gmail.com>
2024-07-23 17:48:31 -0400

    feat(views): create Form view

---
+++ src/views/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../components/Hero/Hero";
+
+function Form() {
+  return (
+    <React.Fragment>
+      <Hero />
+      <h1>formulario</h1>
+    </React.Fragment>
+  );
+}
+
+export default Form;
