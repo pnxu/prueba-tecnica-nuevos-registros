061748bb37acfce8e5b55828dc3e3ecd4b9f081b
pnxu <fcaavv@gmail.com>
2024-07-24 01:36:25 -0400

    feat(MainForm): create MainForm view & add Hero and Form

---
+++ src/views/MainForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/Hero/Hero";
+import Form from "../components/Form/Form";
+
+function MainForm() {
+  return (
+    <React.Fragment>
+      <Hero />
+      <Form />
+    </React.Fragment>
+  );
+}
+
+export default MainForm;
